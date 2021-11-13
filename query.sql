99723a3026df8a732b2a6bdf9eba400936109393
Ivaan <ivan_danilov@ukr.net>
2021-11-14 01:37:44 +0200

    tasks of lab2

--- query.sql
+++ query.sql
@@ -1,21 +1,16 @@
 -- query 1 --
 SELECT format.format_name, count(format.format_name) 
-FROM Comics 
-INNER JOIN Format ON Comics.format_id = Format.format_id
+FROM Comics INNER JOIN Format ON Comics.format_id = Format.format_id
 GROUP BY format.format_name
 
 -- query 2 --
 SELECT publish.publish_data, count(publish.publish_data) 
-FROM Comics 
-INNER JOIN publish ON Comics.publish_id = publish.publish_id
+FROM Comics INNER JOIN publish ON Comics.publish_id = publish.publish_id
 GROUP BY publish.publish_data
 
 -- query 3 --
 SELECT writer_name, COUNT(comics_name)
-FROM Comics
-INNER JOIN Writer ON Comics.writer_id = Writer.writer_id
-JOIN Format ON Comics.format_id = Format.format_id
-where format_name in ('Digest')
-GROUP BY writer_name
-ORDER BY COUNT(comics_name)
+FROM Comics INNER JOIN Writer ON Comics.writer_id = Writer.writer_id
+JOIN Format ON Comics.format_id = Format.format_id where format_name in ('Digest')
+GROUP BY writer_name ORDER BY COUNT(comics_name)
 
